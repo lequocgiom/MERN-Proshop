4b98346569f703d40542205f29cd2b6918fac830
lequocgiom <letrung95@gmail.com>
2020-11-04 22:41:15 +0700

    Product details reducer & action.

--- frontend/src/actions/productAcions.js
+++ frontend/src/actions/productAcions.js
@@ -1,9 +1,13 @@
 import {
+  PRODUCT_DETAIL_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
 } from '../constants/productConstants';
 import axios from 'axios';
+
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({
@@ -19,6 +23,29 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: PRODUCT_DETAIL_REQUEST,
+    });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAIL_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAIL_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
