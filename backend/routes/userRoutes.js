e5888404dc33f8a88c49dc636eccefe219033e28
lequocgiom <letrung95@gmail.com>
2020-11-30 14:22:27 +0700

    Custom authentication middleware.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const authUser = require("../controllers/userController");
+const { authUser, getUserProfile } = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/login").post(authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 module.exports = router;
