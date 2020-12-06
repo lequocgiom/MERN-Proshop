e8689fb6095a95e8e755adc1c2ae7bebca8acdde
lequocgiom <letrung95@gmail.com>
2020-12-06 15:31:52 +0700

    Product Search.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react';
-import { Col, Row } from 'react-bootstrap';
-import Product from '../components/Product';
-import Loader from '../components/Loader';
-import Message from '../components/Message';
-import { useDispatch, useSelector } from 'react-redux';
-import { listProducts } from '../actions/productAcions';
+import React, { useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productAcions";
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword;
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
-  const productList = useSelector((state) => state.productList);
+  const productList = useSelector(state => state.productList);
 
   const { loading, error, products } = productList;
 
@@ -26,7 +27,7 @@ const HomeScreen = () => {
         <Message variant="danger">{error}</Message>
       ) : (
         <Row>
-          {products.map((product) => (
+          {products.map(product => (
             <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
               <Product product={product} />
             </Col>
