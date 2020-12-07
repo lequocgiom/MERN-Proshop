97e467bd56d383e5715b181e82e7810a19426b72
lequocgiom <letrung95@gmail.com>
2020-12-07 21:12:47 +0700

    prepare for deployment.

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running ...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -36,6 +32,18 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirnamee = path.resolve();
 app.use("/uploads", express.static(path.join(__dirnamee, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirnamee, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirnamee, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running ...");
+  });
+}
 
 app.use(notFound);
 
